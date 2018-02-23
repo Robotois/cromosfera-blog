cf28080f731fff5ecfcc32d7a97f413f2e2f743c
yovany-lg <yovanylg@gmail.com>
2018-02-23 12:01:41 -0600

    project started

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hola hola!</h1>
+    <p>Estamos construyendo nuestro blog :D.</p>
+  </div>
+)
+
+export default IndexPage
